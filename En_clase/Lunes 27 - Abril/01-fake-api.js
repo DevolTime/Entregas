79c224a500079306451be2a8febd1669ba82d456
DevolTime <yefriathertua@gmail.com>
2026-04-27 21:19:47 -0500

    implementacion try catch

--- En_clase/Lunes 27 - Abril/01-fake-api.js
+++ En_clase/Lunes 27 - Abril/01-fake-api.js
@@ -1,10 +1,14 @@
 const API = "https://fakeapi.net/products/category/electronics";
 
+
 async function getProducts() {
+    try {
     const response = await fetch (API);
     const data = await response.json();
-
     console.log(data)
+    }  catch (error){
+        console.error("Error al conectarse con la fuente")
+    }
 }
 
 getProducts();
