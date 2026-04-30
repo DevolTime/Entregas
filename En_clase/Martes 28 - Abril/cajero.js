3882a72531d6a1d6e8ede1281a5c4d3bf4723505
DevolTime <yefriathertua@gmail.com>
2026-04-30 00:39:44 -0500

    ejercicio de cajero

---
+++ En_clase/Martes 28 - Abril/cajero.js
@@ -0,0 +1,8 @@
+/* Simular un cajero automático, el cajero puede hacer diversas tareas, al crear la cuenta debe haber un saldo de 100 usd
+1 - Consignar dinero, no se puede consignar valores negativos
+2 - retirar dinero, puede retirar pero debe dejar un saldo del 10% de la apertura de la cuenta
+3 - consultar saldo
+4 - pagar: cobrar el 5%
+5 - transferir: cobrar 1%
+6 - historial de movimientos
+*/
